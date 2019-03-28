7c33165e4cf9974a32556ab2f27e21f662079f2b
John Kallie <JKallie@ApexMicro.com>
2019-03-28 05:05:10 -0700

    initial commit

---
+++ Listener/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listener
+{
+    class Program
+    {
+        private static string topic = "trade-alerts";
+        //private static string subscription = "listener";
+        static void Main(string[] args)
+        {
+
+            if (args.Length > 0)  topic = args[0];
+            Console.WriteLine($"Listening to: {topic}");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("-----   This is a listener   -----");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            var serviceBusConnectionString = ConfigurationManager.AppSettings["serviceBus"];
+
+            //var subscriptionClient = new SubscriptionClient(serviceBusConnectionString, topic, subscription);
+            //subscriptionClient.RegisterMessageHandler(async (msg, cancelationToken) =>
+
+            var queueClient = new QueueClient(serviceBusConnectionString, topic); //, ReceiveMode.PeekLock);
+            queueClient.RegisterMessageHandler(async (msg, exception) =>
+            {
+                var body = Encoding.UTF8.GetString(msg.Body);
+                Console.WriteLine(body);
+
+                await Task.CompletedTask;
+            },
+            async exception =>
+            {
+                await Task.CompletedTask;
+                Console.WriteLine(exception);
+                // log exception
+            }
+            );
+            Console.WriteLine("Hit Enter to Continue - ");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Publisher/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Runtime.Serialization.Json;
+
+namespace Publisher
+{
+    public class Range {
+        public string symbol;
+        public string status;
+        public string trade;
+        public decimal limit;
+        public decimal stop;
+    }
+
+    class Program
+    {
+        private static string topic = "trade-alerts";
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("-----   This is a publisher   -----");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+
+            var loop = true;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine("Write you message:");
+
+                var message = Console.ReadLine();
+                if (message == "q")
+                    break;
+
+                var serviceBusConnectionString = ConfigurationManager.AppSettings["serviceBus"];
+                //var topicClient = new TopicClient(serviceBusConnectionString, topic);
+                var queueClient = new QueueClient(serviceBusConnectionString, topic);
+                Range range = new Range() { symbol = "TEST", limit = 100m, stop = 99m, status = "A-Up", trade = "Buy" };
+                //var body = Encoding.UTF8.GetBytes(message);
+                //var body = Encoding.UTF8.GetBytes(range);
+                //var busMessage = new Message(body);
+                //var json = new <Range>JsonSerializer(range);
+                var busMessage = new Message(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(range)));
+                //topicClient.SendAsync(busMessage).GetAwaiter().GetResult();
+                queueClient.SendAsync(busMessage).GetAwaiter().GetResult();
+                
+
+            } while (loop);
+
+
+        }
+    }
+}
